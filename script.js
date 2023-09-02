ac557bbd8568a0b3b7068e47ae60fc40214296d3
suman-hessa <sumanhessa001@gmail.com>
2023-09-02 21:13:45 +0530

    New javaScript Practice

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 //          clearTimeout(clear);
 //          console.log("The clear time out function was executed !");
 //  }
-
+/*
 function displayDate (){
     date = new Date();
     console.log(date);
@@ -27,3 +27,41 @@ let timeOut = setTimeout(displayDate, 5000);
     clearTimeout(timeOut);
     console.log("The clear timeout function was executed");
  }
+ */
+
+// let newDate = new Date("2003-07-16");
+// console.log(newDate);
+
+// new Date = new Date(year , month, date,  hour , minutes, seconds, milliseconds);
+// let nowDate = new Date(2003,6, 16, 7, 30, 10, 10);
+// console.log(nowDate);
+
+
+// console.log(time);
+
+
+
+// function clock (){
+//     let time = new Date();
+//     let hr = time.getHours();
+// let min = time.getMinutes();
+// let sec = time.getSeconds();
+// console.log(`${hr}:${min}:${sec}`);
+// let clk = document.getElementById('time').innerHTML = (`${hr}:${min}:${sec}`);
+// }
+// let intv = setInterval(clock, 1000);
+
+// function stop (){
+//     clearInterval(intv);
+// }
+
+let now = new Date();
+console.log(now);
+
+now.setDate(20);
+console.log(now);
+
+now.setHours(5);
+console.log(now);
+
+ 
